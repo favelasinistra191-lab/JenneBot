8fa36227e544501e745c76bd90b180fc26987492
favelasinistra191-lab <favelasinistra191@gmail.com>
2026-08-31 13:57:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ import requests
 import config
 import database as db
 
-# Configurações Mercado Pago (Lendo do Render)
+# Configurações Mercado Pago (Oficial)
 import mercadopago
+
 MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+if not MP_ACCESS_TOKEN:
+    # Fallback caso a variável não venha do ambiente do Render
+    MP_ACCESS_TOKEN = "APP_USR-249848378901175-080605-e67c3c2b3575d5a687864a126913a7ae-3171236437"
+
 sdk = mercadopago.SDK(MP_ACCESS_TOKEN)
+
 
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger("DonGhostBot")
